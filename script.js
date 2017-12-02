0e0595121b95819ebd87e9c391ce1ca418290cd5
Michal Bobik <mikebobik1811@gmail.com>
2017-12-02 22:19:37 +0100

    Zadanie_9_6_poprawione

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
-	element.innerHTML = 'item' + ' ' + document.getElementsByTagName('li').length;
+	element.innerHTML = 'item' + ' ' + document.getElementById('list').getElementsByTagName('li').length;
 	list.appendChild(element);
 	console.log(element);
 });
